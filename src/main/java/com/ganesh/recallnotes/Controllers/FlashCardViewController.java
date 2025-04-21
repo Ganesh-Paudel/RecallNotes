f65c84105d04a3a97b87ba5e130ac2ab24a013fa
Ganesh Paudel <paudelganesh080@gmail.com>
2025-04-21 13:10:36 -0400

    Flash Card showing nicely for all the different content

--- src/main/java/com/ganesh/recallnotes/Controllers/FlashCardViewController.java
+++ src/main/java/com/ganesh/recallnotes/Controllers/FlashCardViewController.java
@@ -1,12 +1,15 @@
 package com.ganesh.recallnotes.Controllers;
 
+import com.ganesh.recallnotes.FileHandling.ReadFile;
+import com.ganesh.recallnotes.Main;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
 
 import java.io.File;
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FlashCardViewController{
     @FXML private FlowPane flowFlashCard;
@@ -16,8 +19,31 @@ public class FlashCardViewController{
         this.file = file;
         System.out.println("In the initialData method");
         System.out.println(file.getAbsolutePath());
+
+        addCards();
     }
 
-    
+    private void addCards(){
+        try {
+            ReadFile readFile = new ReadFile(file);
+            String[] section;
+            while ((section = readFile.getSection()) != null) {
+                FXMLLoader loader = new FXMLLoader(Main.class.getResource("FlashCard.fxml"));
+                SingleCardController singleCardController = new SingleCardController();
+                loader.setController(singleCardController);
+                Pane cardPane = loader.load();
+                singleCardController.setTitle(section[0]);
+                singleCardController.setContent(section[1]);
+                flowFlashCard.getChildren().add(cardPane);
+            }
+
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }  catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
