a52a7dca4e99112a426dd2b46931fbefb807c93a
Ganesh Paudel <paudelganesh080@gmail.com>
2025-04-24 11:41:14 -0400

    Color changes and Timer works fine

--- src/main/java/com/ganesh/recallnotes/Controllers/TimerController.java
+++ src/main/java/com/ganesh/recallnotes/Controllers/TimerController.java
@@ -6,6 +6,7 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.ButtonType;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
@@ -24,6 +25,8 @@ public class TimerController {
     @FXML private Text minutesTime;
     @FXML private Text secondsTime;
     @FXML private Button startPauseButton;
+    @FXML private Button breakTimeButton;
+    @FXML private Pane breakOverPane;
 
     private Thread clockThread;
     private boolean running = true;
@@ -31,6 +34,8 @@ public class TimerController {
     private int second = 0;
     private int minute = 0;
     private int hours = 0;
+    private boolean isBreak = false;
+
 
     @FXML
     private void handleGoBack(){
@@ -46,37 +51,55 @@ public class TimerController {
 
     @FXML
     private void startPauseHandler(){
-
-        System.out.println("start Button clicked");
+        if(breakTimeButton.isDisabled()){
+            breakTimeButton.setDisable(false);
+        }
+        if(breakTimeButton.getText().equals("End Break")){
+            breakTimeButton.setText("Break");
+        }
+        if(breakOverPane.isVisible()){
+            breakOverPane.setVisible(false);
+        }
+//        System.out.println("start Button clicked");
         boolean pause = startPauseButton.getText().equals("Pause");
-        startPauseButton.setText(pause ? "Start" : "Pause");
-        running = !pause;
-        startPauseTime(pause);
+        if(!isBreak){
+            startPauseButton.setText(pause ? "Start" : "Pause");
+            running = !pause;
+            startPauseTime(pause);
+        }
 
     }
 
     @FXML
     private void breakTimeHandler(){
-        String title = "Break Time";
-        String headerText = "How long do you want to take a break";
-        String contentText = "5, 10, 15";
-        AlertBox alertBox = new AlertBox("confirmation", title, headerText, contentText);
-        alertBox.addButtons("05", "10", "15");
+        if(breakTimeButton.getText().equals("Break")){
+            running = false;
+            String title = "Break Time";
+            String headerText = "How long do you want to take a break";
+            String contentText = "5, 10, 15";
+            AlertBox alertBox = new AlertBox("confirmation", title, headerText, contentText);
+            alertBox.addButtons("05", "10", "15");
 
-        Optional<ButtonType> result = alertBox.getResult();
-        if(result.isPresent()){
-            if(result.get().getText().equals("05")){
-                System.out.println("break time of 5 minutes");
-            } else if(result.get().getText().equals("10")){
-                System.out.println("break time of 10 minutes");
-            } else if(result.get().getText().equals("15")){
-                System.out.println("break time of 15 minutes");
+            Optional<ButtonType> result = alertBox.getResult();
+            if(result.isPresent()){
+                if(result.get().getText().equals("05")){
+                    startBreakTime(1);
+                } else if(result.get().getText().equals("10")){
+                    startBreakTime(10);
+                } else if(result.get().getText().equals("15")){
+                    startBreakTime(15);
+                }
             }
+            breakTimeButton.setText("End Break");
+            startPauseButton.setText("Start");
+
+        } else if(breakTimeButton.getText().equals("End Break")){
+            running = false;
+            stopTime();
+            breakTimeButton.setText("Break");
         }
 
 
-        running = false;
-        startPauseButton.setText("Start");
 
 
     }
@@ -84,6 +107,12 @@ public class TimerController {
     @FXML
     private void stopTimeHandler(){
         running = false;
+        stopTime();
+    }
+
+    private void stopTime(){
+        breakTimeButton.setText("Break");
+        startPauseButton.setText("Start");
         secondsTime.setText("00");
         minutesTime.setText("00");
         hoursTime.setText("00");
@@ -91,7 +120,7 @@ public class TimerController {
         minute = 0;
         hours = 0;
 
-        clockThread.interrupt();
+//        clockThread.interrupt();
     }
 
 
@@ -100,15 +129,23 @@ public class TimerController {
         clockThread = new Thread(() -> {
             while(running){
                 if(!pause){
-                    System.out.println("inside thread getting seconds");
-                    increaseTime();
-                    System.out.println(second);
+                    if(!isBreak) {
+                        System.out.println("inside thread getting seconds");
+                        increaseTime();
+                        System.out.println(second);
+
+                    } else{
+                        System.out.println("inside thread getting seconds");
+                        decreaseTime();
+                    }
                     Platform.runLater(() -> {
                         secondsTime.setText(getTimeText(second));
                         minutesTime.setText(getTimeText(minute));
                         hoursTime.setText(getTimeText(hours));
                     });
                 }
+
+
 
                 try{
                     Thread.sleep(1000);
@@ -134,8 +171,44 @@ public class TimerController {
         }
     }
 
+    private void decreaseTime(){
+        second--;
+        if(second <= 0 && minute <= 0 && hours <= 0){
+            isBreak = false;
+            running = false;
+            stopTime();
+            setBreakTimePaneVisible();
+            return;
+        }
+        if(second <= 0){
+            second = 59;
+            minute--;
+        }
+        if(minute < 0){
+            second = 59;
+            minute = 0;
+            hours--;
+            if(hours <= 0){
+                hours = 0;
+            }
+        }
+
+    }
+
+    private void setBreakTimePaneVisible(){
+        breakOverPane.setVisible(true);
+    }
+
     private String getTimeText(int time){
         return (""+time).length() < 2 ? "0"+time : ""+time;
+    }
+
+    private void startBreakTime(int time){
+        this.isBreak = true;
+        this.second = 0;
+        this.minute = time;
+        this.running = true;
+        startPauseTime(false);
     }
 
 
